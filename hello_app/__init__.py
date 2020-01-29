56e961177803d83660b3a064560e96a31c59fd24
Ranganathan Veluswamy <rangawillb4u@gmail.com>
2020-01-29 17:37:32 +0000

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,6 @@
 from flask import Flask  # Import the Flask class
 app = Flask(__name__)    # Create an instance of the class for our use
+
+app.config["DEBUG"] = True
+app.config["JSON_SORT_KEYS"] = False
+CORS(app)
